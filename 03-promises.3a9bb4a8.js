1e991071817ec9df538acf03177532f7f0efb75e
Kmaksym1 <106647642+Kmaksym1@users.noreply.github.com>
2023-04-19 16:05:21 +0000

    Deploying to gh-pages from @ Kmaksym1/goit-js-hw-09@7de670a1f3c10ec436dbb0029acc88a7a49af71c 🚀

--- 03-promises.3a9bb4a8.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector(".form");function t(e,t){Math.random()}e.addEventListener("submit",(function(){event.preventDefault();for(var n=e.amount.value,a=e.delay.value,u=(e.step.value,0);u<n;u+=1)t(u,a)}))}();
-//# sourceMappingURL=03-promises.3a9bb4a8.js.map
--- 03-promises.e6864e89.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector(".form");function t(e,t){Math.random()}e.addEventListener("submit",(function(){event.preventDefault();const n=e.amount.value,o=e.delay.value;e.step.value;for(let e=0;e<n;e+=1)t(e,o)}));
-//# sourceMappingURL=03-promises.e6864e89.js.map
